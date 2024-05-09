b85300b13e1172974393bb508a530fb0add78b9f
pcsprashanth <52338785+pcsprashanth@users.noreply.github.com>
2024-05-09 13:21:35 +0000

    second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  member.style.display = "block";
+  var skills = document.getElementById("skills");
+  skills.style.display = "none";
+}
